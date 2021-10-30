78c704962ad3c5e74aa3a39e3243c244116d7712
Willian18nascimento <36906632+Willian18nascimento@users.noreply.github.com>
2021-10-30 03:41:03 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Site Teste Para Aula UFRA</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
